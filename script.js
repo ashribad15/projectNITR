61f49941d8add16a7327e54ec8be30fd19841289
Prafful Awadhiya <136183013+Prafful047@users.noreply.github.com>
2023-07-27 22:28:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,108 @@
+/*const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+let slideIndex = 0;
+
+function showSlide(index) {
+  slider.style.transform = `translateX(-${index * 100}%)`;
+  dots.forEach((dot, i) => dot.classList.toggle("active", i === index));
+}
+
+function prevSlide() {
+  slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+  showSlide(slideIndex);
+}
+
+function nextSlide() {
+  slideIndex = (slideIndex + 1) % slides.length;
+  showSlide(slideIndex);
+}
+
+prevBtn.addEventListener("click", prevSlide);
+nextBtn.addEventListener("click", nextSlide);
+
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    showSlide(i);
+    slideIndex = i;
+  });
+});
+
+// Automatic slideshow
+function autoSlide() {
+  nextSlide();
+}
+
+// Change slide every 5 seconds (adjust as needed)
+const interval = setInterval(autoSlide, 4000);
+
+// Stop auto sliding when hovering over the slider
+slider.addEventListener("mouseenter", () => clearInterval(interval));
+
+// Resume auto sliding when not hovering
+slider.addEventListener("mouseleave", () => {
+  clearInterval(interval);
+  interval = setInterval(autoSlide, 4000);
+});
+
+// Initially display the first slide
+showSlide(slideIndex);*/
+
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+let slideIndex = 0;
+let interval;
+
+function showSlide(index) {
+  slider.style.transform = `translateX(-${index * 100}%)`;
+  dots.forEach((dot, i) => dot.classList.toggle("active", i === index));
+}
+
+function prevSlide() {
+  slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+  showSlide(slideIndex);
+}
+
+function nextSlide() {
+  slideIndex = (slideIndex + 1) % slides.length;
+  showSlide(slideIndex);
+}
+
+prevBtn.addEventListener("click", prevSlide);
+nextBtn.addEventListener("click", nextSlide);
+
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    showSlide(i);
+    slideIndex = i;
+    restartInterval(); // Restart the interval when navigating with dots
+  });
+});
+
+function autoSlide() {
+  nextSlide();
+}
+
+function restartInterval() {
+  clearInterval(interval);
+  interval = setInterval(autoSlide, 3000);
+}
+
+// Start the initial interval
+restartInterval();
+
+// Pause auto sliding when hovering over the slider
+slider.addEventListener("mouseenter", () => clearInterval(interval));
+
+// Resume auto sliding when not hovering
+slider.addEventListener("mouseleave", () => restartInterval());
+
+// Initially display the first slide
+showSlide(slideIndex);
